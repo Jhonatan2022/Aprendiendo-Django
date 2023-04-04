47cf8c43f3f90a7d888244135b87a7ff988f0a01
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-04-04 09:07:52 +0100

    Styles in project

--- djangoproject/myapp/forms.py
+++ djangoproject/myapp/forms.py
@@ -9,16 +9,29 @@ from django import forms
 class Createtask(forms.Form):
 
     # Creamos un campo de tipo CharField para el título de la tarea
-    tittle = forms.CharField(label="Titulo de la tarea", max_length=60)
+    # Agregaremos estilos al formulario
+    tittle = forms.CharField(label="Titulo de la tarea", max_length=60, widget=forms.TextInput(attrs={
+        # Agregaremos estilos al formulario
+        'class': 'form-control',
+        'placeholder': 'Titulo de la tarea'}))
 
     # Creamos un campo de tipo CharField para la descripción de la tarea
     description = forms.CharField(
-        label="Descripción de la tarea", widget=forms.Textarea)
-    
+        label="Descripción de la tarea", widget=forms.Textarea(attrs={
+            # Agregaremos estilos al formulario
+            'class': 'form-control',
+            'placeholder': 'Descripcion de la tarea',
+            'rows': '3'}))
+
     # Creamos un campo para seleccionar el proyecto al que pertenece la tarea o crear uno nuevo proyecto
+
 
 # Crearemos una clase para un formulario para crear un proyecto
 class Createproject(forms.Form):
 
     # Creamos un campo de tipo CharField para el título del proyecto
-    name = forms.CharField(label="Titulo del proyecto", max_length=50)
+    name = forms.CharField(label="Titulo del proyecto", max_length=50, 
+    widget=forms.TextInput(attrs={
+        # Agregaremos estilos al formulario
+        'class': 'form-control',
+        'placeholder': 'Nombre del proyecto'}))
